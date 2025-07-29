15810a1191a772458f69711119aed38511b4d869
Phat Tran <phat.trancompsci@hcmut.edu.vn>
2025-07-29 16:55:39 +0700

    Fix getHistory theo yêu cầu FE v2

--- src/services/discountService.js
+++ src/services/discountService.js
@@ -257,7 +257,6 @@ const discountService = {
         try {
             const request = await DiscountRequestModel.findByIdAndUpdate(id, {
                 status: constant.APPROVAL_STATUS.APPROVED,
-                refundStatus: constant.REFUND_STATUS.UNPAID,
             })
             if (!request) {
                 throw new BadReq(errorCode.DISCOUNT_REQUEST_NOT_FOUND)
